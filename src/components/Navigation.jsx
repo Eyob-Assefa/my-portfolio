b6680d79015a4254a3218bcd8c9984702ba6c4d5
Eyob-Assefa <assefaeyob21@gmail.com>
2025-12-28 02:58:16 +0400

    modified the styling of navigation sidebar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -30,19 +30,42 @@ function Navigation() {
         <Navbar.Toggle aria-controls="sidebar-nav" className="mb-3 border-dark rounded-0" />
         
         <Navbar.Collapse id="sidebar-nav" className="w-100">
-          <Nav className="flex-column w-100 gap-3 align-items-center align-items-md-start">
-            {/* Added 'w-100 text-center' to ensure clickable area is good */}
-            <Nav.Link href="#about" className="text-dark w-100 border-bottom border-dark pb-1 text-center text-md-center">About Me</Nav.Link>
-            <Nav.Link href="#projects" className="text-dark w-100 border-bottom border-dark pb-1 text-center text-md-center">Projects</Nav.Link>
-            <Nav.Link href="#contact" className="text-dark w-100 border-bottom border-dark pb-1 text-center text-md-center">Contact</Nav.Link>
+          <Nav className="flex-column w-100 gap-3 align-items-center">
+            <Nav.Link href="#about" className="nav-link-custom text-center">
+                About Me
+            </Nav.Link>
+
+            <Nav.Link href="#projects" className="nav-link-custom text-center">
+                Projects
+            </Nav.Link>
+
+            <Nav.Link href="#contact" className="nav-link-custom text-center">
+                Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
 
         {/* --- BOTTOM SECTION: Social Icons --- */}
         <div className="mt-auto d-none d-md-flex gap-4 justify-content-center w-100 pt-4">
-           <a href="https://github.com" className="text-dark link-offset-2 link-underline-opacity-0 hover-opacity"><FaGithub size={22} /></a>
-           <a href="https://linkedin.com" className="text-dark link-offset-2 link-underline-opacity-0 hover-opacity"><FaLinkedin size={22} /></a>
-           <a href="mailto:email@example.com" className="text-dark link-offset-2 link-underline-opacity-0 hover-opacity"><FaEnvelope size={22} /></a>
+           <a href="https://github.com"
+              target="_blank"
+              rel="noopener noreferrer" 
+              className="text-dark link-offset-2 link-underline-opacity-0 hover-opacity"
+            >
+                <FaGithub size={22} />
+            </a>
+           <a href="https://linkedin.com" 
+              targe="_blank"
+              rel="noopener noreferrer"
+              className="text-dark link-offset-2 link-underline-opacity-0 hover-opacity"
+           >
+              <FaLinkedin size={22} />
+            </a>
+           <a href="mailto:email@example.com" 
+              className="text-dark link-offset-2 link-underline-opacity-0 hover-opacity"
+           >
+              <FaEnvelope size={22} />
+            </a>
         </div>
 
       </Container>
